3cc0d6067af16de79ef06e653c4a633003a5c7c8
alangogoro <cayne9160@gmail.com>
2019-09-29 22:24:20 +0800

    SignOut button

--- app/src/main/java/com/g2/runningFront/SettingActivity/SettingMainFragment.java
+++ app/src/main/java/com/g2/runningFront/SettingActivity/SettingMainFragment.java
@@ -1,5 +1,6 @@
 package com.g2.runningFront.SettingActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,14 +8,44 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+/* 除錯用 */
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+/* res 目錄 */
+import com.g2.runningFront.R;
+/* UI 元件 */
+import static android.view.View.GONE;
 
 public class SettingMainFragment extends Fragment {
+    private static final String TAG = "TAG_SETmain";
+    private Activity activity;
+    private TextView textView;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        activity.setTitle("登入頁面");
+        return inflater.inflate(R.layout.fragment_setting_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated");
+
+        view.findViewById(R.id.btSignOut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setVisibility(GONE);
+            }
+        });
     }
 }
