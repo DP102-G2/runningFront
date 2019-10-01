a729d0567b9142caa83e723830113709c89a5cdb
alangogoro <cayne9160@gmail.com>
2019-10-01 18:28:13 +0800

    10/01 1830 GoogleSignIn (bundle?)

--- app/src/main/java/com/g2/runningFront/SettingActivity/SettingMainFragment.java
+++ app/src/main/java/com/g2/runningFront/SettingActivity/SettingMainFragment.java
@@ -1,6 +1,7 @@
 package com.g2.runningFront.SettingActivity;
 
 import android.app.Activity;
+import android.content.Intent;// Intent
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,15 +18,32 @@ import android.widget.TextView;
 import com.g2.runningFront.R;// res 目錄
 import static android.view.View.GONE;/// UI
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;// Google 登入
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class SettingMainFragment extends Fragment {
     private static final String TAG = "TAG_SETmain";
     private Activity activity;
+    /* 宣告 GoogleSignInClient */
+    public GoogleSignInClient mGoogleSignInClient;
+    public static final int GOOGLE_SIGNIN = 5;
+
     private TextView textView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = getActivity();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(activity, gso);
+
     }
     @Nullable
     @Override
@@ -39,11 +57,43 @@ public class SettingMainFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.d(TAG, "onViewCreated");
 
+        view.findViewById(R.id.btSignInGoogle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent googleSignInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivity(googleSignInIntent, GOOGLE_SIGNIN);
+
+            }
+        });
+
         view.findViewById(R.id.btSignOut).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 view.setVisibility(GONE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GOOGLE_SIGNIN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try{
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Log.d(TAG, "handleSignInResult getName: " + account.getDisplayName());
+            Log.d(TAG, "handleSignInResult getEmail: " + account.getEmail());
+
+        } catch (ApiException e){
+            Log.w(TAG, "SignInResult: failed code = " + e.getStatusCode());
+        }
     }
 }
