361556352b0ce2e7830134be5e8d7bc532abb232
alangogoro <cayne9160@gmail.com>
2019-10-03 13:52:27 +0800

    10/03 1400 Signin Activity added

--- app/src/main/java/com/g2/runningFront/SignInActivity/SignInActivity.java
+++ app/src/main/java/com/g2/runningFront/SignInActivity/SignInActivity.java
@@ -1,18 +1,26 @@
 package com.g2.runningFront.SignInActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.g2.runningFront.R;
 
 public class SignInActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        setTitle("登入 Activity");
+
+        textView = findViewById(R.id.textView);
+        textView.setText("這裡是 SigninActivity.java");
 
     }
 }
